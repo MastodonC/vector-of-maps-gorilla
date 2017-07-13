6a39a0965fc92c4d5ee9589568591439cb1e3dd2
Elise Huard <elise@mastodonc.com>
2017-07-13 12:32:20 +0100

    preserving order of columns when given

--- src/vector_of_maps_gorilla/render.clj
+++ src/vector_of_maps_gorilla/render.clj
@@ -22,7 +22,7 @@
                            flatten
                            set)
          columns (if cols
-                   (clojure.set/intersection (set cols) column-names)
+                   (filter #(some #{%} column-names) cols)
                    column-names)
          rendfn (fn [open close sep r] (list-like (map render/render r) (pr-str r) open close sep))
          rows (map (partial rendfn "<tr><td>" "</td></tr>" "</td><td>")
